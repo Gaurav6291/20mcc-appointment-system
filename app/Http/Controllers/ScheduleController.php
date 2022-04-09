d248da943d350d55f03daa25fd81e607111b84d9
Gaurav <parmargaurav1234@gmail.com>
2022-04-09 10:55:59 +0530

    code splitted in different controller

--- app/Http/Controllers/ScheduleController.php
+++ app/Http/Controllers/ScheduleController.php
@@ -2,84 +2,279 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Schedule;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Storage;
+use App\Models\Schedule;
 
 class ScheduleController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
+    //show project types -- 1.) Residential, 2.) commercial, 3.) industrial 
+    public function project_type(){
+        return view('schedule.project_type');
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+    //after selecting project type, redirect to its subtype -- 1.) waterproofing, 2.) constructional application
+    //for remembering the previous options, i am storing the data in session
+    public function third_screen(Request $request, $id){
+        if($id == '1'){
+            $project_type = "residential";
+        }
+        else if($id == '2'){
+            $project_type = "commercial";
+        }else if($id == '3'){
+            $project_type = "industrial";
+        }
+        $session = $request->session()->put('project-type', $project_type);
+        
+        return view('schedule.third_screen', [
+            'project_type'=> $project_type,
+        ]);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
+    //after selecting subtype, show its form to select further options.
+    public function fourth_screen(Request $request, $id){
+        
+        if($id == '1'){
+            $third_screen = "waterproofing";
+            $session = $request->session()->put('third-screen', $third_screen);
+            return view('schedule.waterproofing_form');
+        }
+        else if($id == '2'){
+            $third_screen = "construction application";
+            $session = $request->session()->put('third-screen', $third_screen);
+            return view('schedule.construction_application_form');
+        }
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Schedule  $schedule
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Schedule $schedule)
-    {
-        //
+    // show the page from which we can select the areas.
+    public function fifth_screen(Request $request, $id){
+        $page = $request->session()->get('third-screen');
+
+        if($id == '1' && $page == 'waterproofing'){
+                $area = "Below 200 sqft";
+                $session = $request->session()->put('area', $area);
+                return view('schedule.waterproofing_types', [
+                'area' => $area,
+            ]);
+        }
+        else if($id == '2' && $page == 'waterproofing'){
+                $area = "200 sqft to 500 sqft";
+                $session = $request->session()->put('area', $area);
+                return view('schedule.waterproofing_types', [
+                'area' => $area,
+            ]);
+        }
+        else if($id == '3' && $page == 'waterproofing'){
+                $area = "500 to 1000 sqft";
+                $session = $request->session()->put('area', $area);
+                return view('schedule.waterproofing_types', [
+                'area' => $area,
+            ]);
+        }
+        else if($id == '4' && $page == 'waterproofing'){
+                $area = "1000sqft to 2000 sqft";
+                $session = $request->session()->put('area', $area);
+                return view('schedule.waterproofing_types', [
+                'area' => $area,
+            ]);
+        }
+        else if($id == '5' && $page == 'waterproofing'){
+                $area = "Above 2000 sqft";
+                $session = $request->session()->put('area', $area);
+                return view('schedule.waterproofing_types', [
+                'area' => $area,
+            ]);
+        }
+        
+        if($id == '1' && $page == 'construction application'){
+                $area = "Below 500 Sqft";
+                $session = $request->session()->put('type', $area);
+                return view('schedule.construction_application_type', [
+                'area' => $area,
+            ]);
+        }
+        else if($id == '2' && $page == 'construction application'){
+                $area = "500 Sqft to 1500 Sqft";
+                $session = $request->session()->put('area', $area);
+                return view('schedule.construction_application_type', [
+                'area' => $area,
+            ]);
+        }
+        else if($id == '3' && $page == 'construction application'){
+                $area = "1500 Sqft to 2500 Sqft";
+                $session = $request->session()->put('area', $area);
+                return view('schedule.construction_application_type', [
+                'area' => $area,
+            ]);
+        }
+        else if($id == '4' && $page == 'construction application'){
+                $area = "2500 Sqft to 5000 Sqft";
+                $session = $request->session()->put('type', $area);
+                return view('schedule.construction_application_type', [
+                'area' => $area,
+            ]);
+        }
+        else if($id == '5' && $page == 'construction application'){
+                $area = "5000 Sqft";
+                $session = $request->session()->put('area', $area);
+                return view('schedule.construction_application_type', [
+                'area' => $area,
+            ]);
+        }
+    }
+    
+    //now on the basis of selected option, let the user to select related images
+    public function sixth_screen(Request $request, $id){
+        $page = $request->session()->get('third-screen');
+        if($id == '1' && $page == 'waterproofing'){
+                $type = "Wall waterproofing";
+                $session = $request->session()->put('type', $type);
+                return view('schedule.img_form', [
+                'type' => $type,
+            ]);
+        }
+        else if($id == '2' && $page == 'waterproofing'){
+                $type = "Roof waterproofing";
+                $session = $request->session()->put('type', $type);
+                return view('schedule.img_form', [
+                'type' => $type,
+            ]);
+        }
+        else if($id == '3' && $page == 'waterproofing'){
+                $type = "Pool Waterproofing";
+                $session = $request->session()->put('type', $type);
+                return view('schedule.img_form', [
+                'type' => $type,
+            ]);
+        }
+        else if($id == '4' && $page == 'waterproofing'){
+                $type = "Ceiling Waterproofing";
+                $session = $request->session()->put('type', $type);
+                return view('schedule.img_form', [
+                'type' => $type,
+            ]);
+        }
+        else if($id == '5' && $page == 'waterproofing'){
+                $type = "Pavement Waterproofing";
+                $session = $request->session()->put('type', $type);
+                return view('schedule.img_form', [
+                'type' => $type,
+            ]);
+        }
+        else if($id == '6' && $page == 'waterproofing'){
+                $type = "Dampness";
+                $session = $request->session()->put('type', $type);
+                return view('schedule.img_form', [
+                'type' => $type,
+            ]);
+        }
+        else if($id == '7' && $page == 'waterproofing'){
+                $type = "Cracks Filling";
+                $session = $request->session()->put('type', $type);
+                return view('schedule.img_form', [
+                'type' => $type,
+            ]);
+        }
+        else if($id == '8' && $page == 'waterproofing'){
+                $type = "Molds";
+                $session = $request->session()->put('type', $type);
+                return view('schedule.img_form', [
+                'type' => $type,
+            ]);
+        }
+        else if($id == '9' && $page == 'waterproofing'){
+                $type = "Paint Peel-Off";
+                $session = $request->session()->put('type', $type);
+                return view('schedule.img_form', [
+                'type' => $type,
+            ]);
+        } 
+        if($id == '1' && $page == 'construction application'){
+                $type = "Masonary Work";
+                $session = $request->session()->put('type', $type);
+                return view('schedule.img_form', [
+                'type' => $type,
+            ]);
+        }
+        else if($id == '2' && $page == 'construction application'){
+                $type = "Plastering Area";
+                $session = $request->session()->put('type', $type);
+                return view('schedule.img_form', [
+                'type' => $type,
+            ]);
+        }
+        else if($id == '3' && $page == 'construction application'){
+                $type = "Slabs & Water Leakage";
+                $session = $request->session()->put('type', $type);
+                return view('schedule.img_form', [
+                'type' => $type,
+            ]);
+        }
+        else if($id == '4' && $page == 'construction application'){
+                $type = "High Strength Construction";
+                $session = $request->session()->put('type', $type);
+                return view('schedule.img_form', [
+                'type' => $type,
+            ]);
+        }
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Schedule  $schedule
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Schedule $schedule)
-    {
-        //
+    // after selecting image, store the image in our local machine, also lets ask the date and time for appointment. 
+    public function img_submit(Request $request){
+        $image_1="";
+        if($request -> hasFile('image_1')){
+            $extension = $request -> file('image_1') -> extension();
+            $file  = $request->file('image_1');
+            $fileNameString = (string) Str::uuid();
+            $image_1 = $fileNameString.".".$extension;
+            $path = Storage::putFileAs('public/image', $file, $image_1);      
+        $session = $request->session()->put('image_1', $image_1);
+        }
+
+        $image_2="";
+        if($request -> hasFile('image_2')){
+            $extension = $request -> file('image_2') -> extension();
+            $file  = $request->file('image_2');
+            $fileNameString = (string) Str::uuid();
+            $image_2 = $fileNameString.".".$extension;
+            $path = Storage::putFileAs('public/image', $file, $image_2);         
+            $session = $request->session()->put('image_2', $image_2);
+
+        }
+
+        $image_3="";
+        if($request -> hasFile('image_3')){
+            $extension = $request -> file('image_3') -> extension();
+            $file  = $request->file('image_3');
+            $fileNameString = (string) Str::uuid();
+            $image_3 = $fileNameString.".".$extension;
+            $path = Storage::putFileAs('public/image', $file, $image_3);      
+            $session = $request->session()->put('image_3', $image_3);
+
+        }
+
+        return view('schedule.calander');
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Schedule  $schedule
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Schedule $schedule)
-    {
-        //
-    }
+    //now lets store all the data of the user in our database. also after this redirect to our main page with success notification. 
+    public function final_submit(Request $request, $id){   
+        $post = new Schedule([
+            'project_type' => $request->session()->get('project-type'),
+            'third_screen' => $request->session()->get('third-screen'),
+            'area' => $request->session()->get('area'),
+            'type' => $request->session()->get('type'),
+            'image_1' => $request->session()->get('image_1'),
+            'image_2' => $request->session()->get('image_2'),
+            'image_3' => $request->session()->get('image_3'),
+            'user_id' => $id,    
+            'start_date' => $request->input('start_date'),    
+            'start_time' => $request->input('start_time'),    
+            'end_date' => $request->input('end_date'),    
+            'end_time' => $request->input('end_time'),    
+        ]);
+        $post->save();
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Schedule  $schedule
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Schedule $schedule)
-    {
-        //
+        return redirect("/")->with('message', 'Thank You!');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ScheduleController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -22,28 +24,35 @@ Route::get('/', function () {
 
 Route::group(['middleware'=>'guest'], function(){
 
+//login screen
 Route::get('login', [Authcontroller::class, 'index'])->name('login');
 Route::post('login', [Authcontroller::class, 'login'])->name('login')->middleware('throttle:2,1');
-
+//registration screen
 Route::get('register', [Authcontroller::class, 'register_view'])->name('register');
 Route::post('register', [Authcontroller::class, 'register'])->name('register');
+
+
 });
 
 //when user is register and logged in
 
 Route::group(['middleware'=>'auth'], function(){
+
+
 Route::get('home', [AuthController::class, 'home'])->name('home');
 Route::get('logout', [AuthController::class, 'logout'])->name('logout');
 
-Route::get('project_type', [AuthController::class, 'project_type']);
-Route::get('third_screen/{id}', [AuthController::class, 'third_screen'])->name('third_screen');
-Route::get('fourth_screen/{id}', [AuthController::class, 'fourth_screen'])->name('third_screen');;
-Route::get('fifth_screen/{id}', [AuthController::class, 'fifth_screen'])->name('fifth_screen');;
-Route::get('sixth_screen/{id}', [AuthController::class, 'sixth_screen'])->name('sixth_screen');;
-Route::post('/img_submit', [AuthController::class, 'img_submit'])->name('img.submit');;
+Route::get('project_type', [ScheduleController::class, 'project_type']);
+Route::get('third_screen/{id}', [ScheduleController::class, 'third_screen'])->name('third_screen');
+Route::get('fourth_screen/{id}', [ScheduleController::class, 'fourth_screen'])->name('third_screen');;
+Route::get('fifth_screen/{id}', [ScheduleController::class, 'fifth_screen'])->name('fifth_screen');;
+Route::get('sixth_screen/{id}', [ScheduleController::class, 'sixth_screen'])->name('sixth_screen');;
+Route::post('/img_submit', [ScheduleController::class, 'img_submit'])->name('img.submit');;
 
-Route::get('fullcalender', [AuthController::class, 'indexx']);
-Route::post('fullcalenderAjax', [AuthController::class, 'ajax']);
+Route::get('fullcalender', [ScheduleController::class, 'indexx']);
+Route::post('fullcalenderAjax', [ScheduleController::class, 'ajax']);
 
-Route::post('final_submit/{id}', [AuthController::class, 'final_submit']);
+Route::post('final_submit/{id}', [ScheduleController::class, 'final_submit']);
+
+
 });
